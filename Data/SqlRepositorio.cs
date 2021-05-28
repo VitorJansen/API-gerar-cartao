d0bf144c6633acd1851523da7d9da325b56e3e69
Vitor Jansen <vitorvieira.id@gmail.com>
2021-05-28 17:22:09 -0300

    feat: Implementação da classe SqlRepositorio

--- Data/SqlRepositorio.cs
+++ Data/SqlRepositorio.cs
@@ -1,10 +1,17 @@
 using System.Collections.Generic;
+using System.Linq;
 using VaiVoaAPI.Models;
 
 namespace VaiVoaAPI.Data
 {
     public class SqlRepositorio : IRepositorio
     {
+        private readonly CartoesContext _context;
+
+        public SqlRepositorio(CartoesContext context)
+        {
+            _context = context;
+        }
         public Cartao CriarNovoCartao(string email)
         {
             throw new System.NotImplementedException();
@@ -12,7 +19,10 @@ namespace VaiVoaAPI.Data
 
         public IEnumerable<Cartao> ListarCartoes(string email)
         {
-            throw new System.NotImplementedException();
+            return _context.Cartoes
+                .ToList()
+                .Where(e => e.Email == email)
+                .OrderBy(i => i.Id);
         }
 
         public bool SaveChanges()
